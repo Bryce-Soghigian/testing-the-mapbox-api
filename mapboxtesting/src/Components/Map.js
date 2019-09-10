09f451a985c3ca2051ef1e7704197882e9871a85
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-10 06:49:29 -0500

    Making it work within react

--- mapboxtesting/src/Components/Map.js
+++ mapboxtesting/src/Components/Map.js
@@ -1,27 +1,37 @@
-// import React from 'react'
-// import './Map.css'
+import React from 'react'
+import mapboxgl from 'mapbox-gl'
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
-// <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.3.1/mapbox-gl.js'></script>
-// <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.3.1/mapbox-gl.css' rel='stylesheet' />
+export default function Map() {
+  const [state,setState] = useState({
+    lng: 5,
+    lat: 34,
+    zoom: 1.5
+  })
+useEffect(() => {
+const {lng, lat, zoom} = state;
 
+const map = new mapboxgl.Map({
+  container: this.mapContainer,
+  style: 'mapbox://styles/mapbox/streets-v9',
+  center: [lng, lat],
+  zoom
+});
+setState({
+  lng: lng.toFixed(4),
+  lat: lat.toFixed(4),
+  zoom: map.getZoom().toFixed(2)
+})
+},[])
 
-
-
- 
-//  export default function Map() {
-//      return (
-// <div id='map'>
-// <div id='menu'>
-// <input id='streets-v11' type='radio' name='rtoggle' value='streets' checked='checked'>
-// <label for='streets'>streets</label>
-// <input id='light-v10' type='radio' name='rtoggle' value='light'>
-// <label for='light'>light</label>
-// <input id='dark-v10' type='radio' name='rtoggle' value='dark'>
-// <label for='dark'>dark</label>
-// <input id='outdoors-v11' type='radio' name='rtoggle' value='outdoors'>
-// <label for='outdoors'>outdoors</label>
-// <input id='satellite-v9' type='radio' name='rtoggle' value='satellite'>
-// <label for='satellite'>satellite</label>
-// </div>
-//  </div>
-    
+  return (
+    <div>
+            <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+      </div>
+    </div>
+  )
+}
