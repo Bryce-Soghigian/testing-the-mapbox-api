f108ac367845e59543e48965b645e6531fce75e6
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-12 15:54:56 -0500

    Rendering map in react

--- mapboxtesting/src/App.js
+++ mapboxtesting/src/App.js
@@ -1,46 +1,62 @@
 import React, {Component} from 'react'
-import Geocoder from 'react-mapbox-gl-geocoder'
-import ReactMapGL from 'react-map-gl'
+import mapboxgl from 'mapbox-gl';
+import './App.css'
+import 'mapbox-gl/dist/mapbox-gl.css';
+// import 'mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.1/mapbox-gl-geocoder.css';
+var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
 
 
-const mapAccess = {
-    mapboxApiAccessToken: 'pk.eyJ1IjoiYnNvZ2hpZ2lhbiIsImEiOiJjazBhOTUxam4wMDNtM2RvNXJzbjQ5aGV6In0.eL8NJ0-ikx_5Dl49994bGw'
-}
 
-const mapStyle = {
-    width: '100%',
-    height: 600
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnNvZ2hpZ2lhbiIsImEiOiJjazBhOTUxam4wMDNtM2RvNXJzbjQ5aGV6In0.eL8NJ0-ikx_5Dl49994bGw';
 
-const queryParams = {
-    country: 'us'
-}
+class App extends Component {
 
-export default class App extends Component {
-    state = {
-        viewport: {}
+  constructor(props) {
+      super(props);
+      this.state = {
+        lng: -97,
+        lat: 50,
+        zoom: 1
+        
+      };
+
     }
 
-    onSelected = (viewport, item) => {
-        this.setState({viewport});
-        console.log('Selected: ', item)
-    }
+  componentDidMount() {
+      const { lng, lat, zoom } = this.state;
 
-    render() {
-        const {viewport} = this.state
+      var map = new mapboxgl.Map({
+        container: 'map',
+        center: [lng, lat],
+        style: 'mapbox://styles/bsoghigian/ck0eubu3v0wzj1cp3eu3khs2e',
+        zoom: zoom
+      });
+      map.addControl(new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+       mapboxgl: mapboxgl
+       }));
+      map.on('move', () => {
+        const { lng, lat } = map.getCenter();
 
-        return (
-            <div>
-                <Geocoder
-                    {...mapAccess} onSelected={this.onSelected} viewport={viewport} hideOnSelect={true}
-                    queryParams={queryParams}
-                />
+        this.setState({
+          lng: lng.toFixed(4),
+          lat: lat.toFixed(4),
+          zoom: map.getZoom().toFixed(2)
+        });
+      });
+  }
 
-                <ReactMapGL
-                    {...mapAccess} {...viewport} {...mapStyle}
-                    onViewportChange={(newViewport) => this.setState({viewport: newViewport})}
-                />
+  render() {
+      const { lng, lat, zoom } = this.state;
+      return (
+        <div className="App">
+            <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+              <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
             </div>
-        )
-    }
+          <div id="map" />
+        </div>
+      );
+  }
 }
+
+export default App;
