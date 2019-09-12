a8b31de791a83c4c93348800c824b365237c79be
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-12 16:32:56 -0500

    Added bounds to react map

--- mapboxtesting/src/App.js
+++ mapboxtesting/src/App.js
@@ -10,53 +10,54 @@ var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnNvZ2hpZ2lhbiIsImEiOiJjazBhOTUxam4wMDNtM2RvNXJzbjQ5aGV6In0.eL8NJ0-ikx_5Dl49994bGw';
 
 class App extends Component {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+          lng: 5,
+          lat: 34,
+          zoom: 1.5
+        };
+      }
+    
+      componentDidMount() {
+        const { lng, lat, zoom } = this.state;
+        const bounds = [
+            [-135, 13], // Southwest coordinates
+[-63, 50] 
+        ]
 
-  constructor(props) {
-      super(props);
-      this.state = {
-        lng: -97,
-        lat: 50,
-        zoom: 1
         
-      };
-
-    }
-
-  componentDidMount() {
-      const { lng, lat, zoom } = this.state;
-
-      var map = new mapboxgl.Map({
-        container: 'map',
-        center: [lng, lat],
-        style: 'mapbox://styles/bsoghigian/ck0eubu3v0wzj1cp3eu3khs2e',
-        zoom: zoom
-      });
-      map.addControl(new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-       mapboxgl: mapboxgl
-       }));
-      map.on('move', () => {
-        const { lng, lat } = map.getCenter();
-
-        this.setState({
-          lng: lng.toFixed(4),
-          lat: lat.toFixed(4),
-          zoom: map.getZoom().toFixed(2)
+        const map = new mapboxgl.Map({
+          container: this.mapContainer,
+          style: 'mapbox://styles/bsoghigian/ck0eubu3v0wzj1cp3eu3khs2e',
+          center: [lng, lat],
+          zoom:zoom,
+          maxBounds: bounds
         });
-      });
-  }
-
-  render() {
-      const { lng, lat, zoom } = this.state;
-      return (
-        <div className="App">
+    
+        map.on('move', () => {
+          const { lng, lat } = map.getCenter();
+    
+          this.setState({
+            lng: lng.toFixed(4),
+            lat: lat.toFixed(4),
+            zoom: map.getZoom().toFixed(2)
+          });
+        });
+      }
+    
+      render() {
+        const { lng, lat, zoom } = this.state;
+    
+        return (
+          <div>
             <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
               <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
             </div>
-          <div id="map" />
-        </div>
-      );
-  }
-}
+            <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+          </div>
+        );
+      }
+    }
 
 export default App;
