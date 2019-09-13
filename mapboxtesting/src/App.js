d8fd38681052ac0eafd7c62383a7ec4b90977d5c
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-13 07:00:50 -0500

    set up for our labs project

--- mapboxtesting/src/App.js
+++ mapboxtesting/src/App.js
@@ -1,67 +1,27 @@
-import React, {Component} from 'react'
-import mapboxgl from 'mapbox-gl';
-import './App.css'
-import 'mapbox-gl/dist/mapbox-gl.css';
-// import 'mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.1/mapbox-gl-geocoder.css';
-var MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
+import React,{useState,useEffect} from 'react'
+import {TokenContext} from './Components/Context/Contexts';
+import {Route,Link} from 'react-router-dom';
+import Map from './Components/Map';
+import Signup from './Components/SignUp/Signup';
+import Login from './Components/Login/Login';
+
+export default function App() {
+const [token,setToken] = useState(false);
+useEffect(() => {
+    setToken(localStorage.getItem("token"));
+    console.log("test");
+  }, [token]); 
 
 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYnNvZ2hpZ2lhbiIsImEiOiJjazBhOTUxam4wMDNtM2RvNXJzbjQ5aGV6In0.eL8NJ0-ikx_5Dl49994bGw';
-
-class App extends Component {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-          lng: 5,
-          lat: 34,
-          zoom: 1.5
-        };
-      }
-    
-      componentDidMount() {
-        const { lng, lat, zoom } = this.state;
-        const bounds = [
-            [-130, 22], // Southwest coordinates
-[-63, 50] 
-        ]
-
-        
-        const map = new mapboxgl.Map({
-          container: this.mapContainer,
-          style: 'mapbox://styles/bsoghigian/ck0eubu3v0wzj1cp3eu3khs2e',
-          center: [lng, lat],
-          zoom:zoom,
-          maxBounds: bounds
-        });
-        map.addControl(new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl
-            }));
-    
-        map.on('move', () => {
-          const { lng, lat } = map.getCenter();
-    
-          this.setState({
-            lng: lng.toFixed(4),
-            lat: lat.toFixed(4),
-            zoom: map.getZoom().toFixed(2)
-          });
-        });
-      }
-    
-      render() {
-        const { lng, lat, zoom } = this.state;
-    
-        return (
-          <div>
-            <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
-              <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
-            </div>
-            <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
-          </div>
-        );
-      }
-    }
-
-export default App;
+return (
+  
+    <div>
+        <TokenContext.Provider value={{token, setToken}}>
+          <Map/>
+          <Route exact path="/"/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path='/signup' component={Signup}/>  
+        </TokenContext.Provider>
+    </div>
+    )
+}
